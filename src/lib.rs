e34dfae823346976ff553521df8b80dc235beb6f
SpieringsAE <ben_spierings@hotmail.com>
2023-07-18 11:32:08 +0200

    added tests, added breakpoint match detection

--- src/lib.rs
+++ src/lib.rs
@@ -90,9 +90,12 @@ where f64: From<T> + From<U>{
     pub fn lookup<Y: Copy>(&self, breakpoint: &Y, extrapolation: Extrapolation, interpolation: Interpolation) -> Result<U, ExtrapolationError>
     where T: From<Y> + From<i8>{
         let calc_breakpoint = T::from(*breakpoint);
-        match self.breakpoints.iter().position(|bp| bp > &calc_breakpoint){ 
+        match self.breakpoints.iter().position(|bp| bp >= &calc_breakpoint){ 
             Some(index) => {
-                if index != 0 {
+                if self.breakpoints.get(index).unwrap() == &calc_breakpoint {
+                    return Ok(*self.values.get(index).unwrap()) 
+                }
+                else if index != 0 {
                     // handle interpolation
                     return match interpolation {
                         Interpolation::Linear => {
@@ -183,7 +186,7 @@ where f64: From<T> + From<U>{
 /// ```
 /// # #[macro_use] extern crate go_lookup_tables; fn main() {
 /// use::go_lookup_tables::*;
-/// let lookup_table = create_1d_lookup!((0u16,500,4500,5000), (0f32,0.0,500.0,500.0)); //simple 0.5V to 4.5V pressure sensor
+/// let lookup_table = create_1d_lookup!((0i16,500,4500,5000), (0f32,0.0,500.0,500.0)); //simple 0.5V to 4.5V pressure sensor
 /// # }
 /// ```
 #[macro_export]
@@ -211,34 +214,45 @@ mod tests {
     use super::OneDLookup;
 
     #[test]
-    fn linear_extrapolation_signed() {
+    fn extrapolation_linear() {
         let lookup_table = create_1d_lookup!((0i16,5000),(0f32,500.0));
-        let result = lookup_table.lookup(&2500i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
         let result1 = lookup_table.lookup(&-1000i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
         let result2 = lookup_table.lookup(&6000i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
-        println!("sensor is an automotive 0 - 5V 0 - 500 bar pressure sensor");
-        println!("linear extrapolation and interpolation enabled");
-        println!("Result at 2.5V is : {}bar", result);
-        assert_eq!(result, 250f32);
-        println!("Result at -1V is:   {}bar", result1);
         assert_eq!(result1, -100f32);
-        println!("Result at 6V is:    {}bar", result2);
         assert_eq!(result2, 600f32);
     }
+
     #[test]
-    fn linear_extrapolation_unsigned() {
-        let lookup_table = create_1d_lookup!((1000i16,5000),(0f32,500.0));
-        let result = lookup_table.lookup(&3000i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
-        let result1 = lookup_table.lookup(&500i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
-        let result2 = lookup_table.lookup(&6000i16, crate::Extrapolation::Linear, crate::Interpolation::Linear).unwrap();
-        println!("sensor is an automotive 1 - 5V 0 - 500 bar pressure sensor");
-        println!("linear extrapolation and interpolation enabled");
-        println!("Result at 3V is :    {}bar", result);
-        assert_eq!(result, 250f32);
-        println!("Result at 0.5V is:   {}bar", result1);
-        assert_eq!(result1, -62.5f32);
-        println!("Result at 6V is:     {}bar", result2);
-        assert_eq!(result2, 625f32);
+    fn extrapolation_none_hold() {
+        let lookup_table = create_1d_lookup!((0i16,5000),(0f32,500.0));
+        let result1 = lookup_table.lookup(&-1000i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        let result2 = lookup_table.lookup(&6000i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        assert_eq!(result1, 0f32);
+        assert_eq!(result2, 500f32);
+    }
+
+    #[test]
+    fn extrapolation_none_error() {
+        let lookup_table = create_1d_lookup!((0i16,5000),(0f32,500.0));
+        let result1 = lookup_table.lookup(&-1000i16, crate::Extrapolation::NoneError, crate::Interpolation::Linear);
+        let result2 = lookup_table.lookup(&6000i16, crate::Extrapolation::NoneError, crate::Interpolation::Linear);
+        let result3 = lookup_table.lookup(&2500i16, crate::Extrapolation::NoneError, crate::Interpolation::Linear);
+        assert!(result1.is_err());
+        assert!(result2.is_err());
+        assert!(result3.is_ok());
+    }
+
+    #[test]
+    fn interpolation_linear() {
+        let lookup_table = create_1d_lookup!((0i16,500,4500,5000), (0i32,0,500,500));
+        let result = lookup_table.lookup(&500i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        let result1 = lookup_table.lookup(&505i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        let result2 = lookup_table.lookup(&4495i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        let result3 = lookup_table.lookup(&2500i16, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::Linear).unwrap();
+        assert_eq!(result, 0i32);
+        assert_eq!(result1, 0i32);
+        assert_eq!(result2, 499i32);
+        assert_eq!(result3, 250i32);
     }
 
     #[test]
@@ -250,5 +264,23 @@ mod tests {
         assert_eq!(result, 6i8); //value is 3.5, round up to 6
         assert_eq!(result1, 6i8); //value is 4.75, round up to 6
         assert_eq!(result2, 1i8); //value is 2.25, round down to 1
+    }
+
+    #[test]
+    fn interpolation_floor() {
+        let lookup_table = create_1d_lookup!((0i8,2,5,6),(0i8,1,6,8));
+        let result = lookup_table.lookup(&0i8, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::NoneFloor).unwrap();
+        let result1 = lookup_table.lookup(&1i8, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::NoneFloor).unwrap();
+        assert_eq!(result, 0i8);
+        assert_eq!(result, result1);
+    }
+
+    #[test]
+    fn interpolation_ceiling() {
+        let lookup_table = create_1d_lookup!((0i8,2,5,6),(0i8,1,6,8));
+        let result = lookup_table.lookup(&3i8, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::NoneCeiling).unwrap();
+        let result1 = lookup_table.lookup(&4i8, crate::Extrapolation::NoneHoldExtreme, crate::Interpolation::NoneCeiling).unwrap();
+        assert_eq!(result, 6i8);
+        assert_eq!(result, result1);
     }
 }
